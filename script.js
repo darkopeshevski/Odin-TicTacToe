82168e6785f7f0e771a12a4f5bd48a2c7f4e5208
Darko Peshevski <darko.peshevski2@gmail.com>
2024-01-21 20:45:23 +0100

    Added 2 separate functions: one for checking if there are 3 fields of the same symbol, and the other one for checking to which player those symbols belong. # Please enter the commit message for your changes. Lines starting

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ function Gameboard() {
   ];
 
   const getBoard = () => board;
+
   const setCell = (row, col, value) => {
     if (row >= 0 && row < 3 && col >= 0 && col < 3 && (board[row][col] == '_' || board[row][col] === 'X' || board[row][col] === '0')) {
       board[row][col] = value;
@@ -25,11 +26,48 @@ function Gameboard() {
     }
   };
 
-  function printBoardVertically() {
-    console.log(`${board[0]}\n${board[1]}\n${board[2]}`);
-  }
+  const printBoardVertically = () => {
+  console.log(`${board[0]}\n${board[1]}\n${board[2]}`);
+  };
 
-  return { getBoard, setCell, printBoardVertically};
+  const checkForWin = () => {
+    // HORIZONTAL ROW CHECK
+    if ((board[0][0] == 'X' && board[0][1] == 'X' && board[0][2] == 'X') || (board[0][0] == '0' && board[0][1] == '0' && board[0][2] == '0')) {
+      return true;
+    }
+    else if ((board[1][0] == 'X' && board[1][1] == 'X' && board[1][2] == 'X') || (board[1][0] == '0' && board[1][1] == '0' && board[1][2] == '0')) {
+      return true;
+    }
+    else if ((board[2][0] == 'X' && board[2][1] == 'X' && board[2][2] == 'X') || (board[2][0] == '0' && board[2][1] == '0' && board[2][2] == '0')) {
+      return true;
+    }
+
+    // VERTICAL ROW CHECK
+    else if ((board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') || (board[0][0] == '0' && board[1][0] == '0' && board[2][0] == '0')) {
+      return true;
+    }
+    else if ((board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') || (board[0][1] == '0' && board[1][1] == '0' && board[2][1] == '0')) {
+      return true;
+    }
+    else if ((board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') || (board[0][2] == '0' && board[1][2] == '0' && board[2][2] == '0')) {
+      return true;
+    }
+
+    // DIAGONAL ROW CHECK
+    else if ((board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') || (board[0][0] == '0' && board[1][1] == '0' && board[2][2] == '0')) {
+      return true;
+    }
+    else if ((board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') || (board[0][2] == '0' && board[1][1] == '0' && board[2][0] == '0')) {
+      return true;
+    }
+
+    else {
+      return false;
+    }
+
+  };
+
+  return { getBoard, setCell, printBoardVertically, checkForWin };
 
 }
 
@@ -62,14 +100,74 @@ function Game() {
     gameBoard.printBoardVertically();
 };
 
+  const checkWinner = () => {
+    // FIRST GROUP
+    if (gameBoard.getBoard()[0][0] == 'X' && gameBoard.getBoard()[0][1] == 'X' && gameBoard.getBoard()[0][2] == 'X') {
+      console.log(`${playerOne.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[0][0] == '0' && gameBoard.getBoard()[0][1] == '0' && gameBoard.getBoard()[0][2] == '0') {
+      console.log(`${playerTwo.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[1][0] == 'X' && gameBoard.getBoard()[1][1] == 'X' && gameBoard.getBoard()[1][2] == 'X') {
+      console.log(`${playerOne.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[1][0] == '0' && gameBoard.getBoard()[1][1] == '0' && gameBoard.getBoard()[1][2] == '0') {
+      console.log(`${playerTwo.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[0][2] == 'X' && gameBoard.getBoard()[1][2] == 'X' && gameBoard.getBoard()[2][2] == 'X') {
+      console.log(`${playerOne.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[2][0] == '0' && gameBoard.getBoard()[2][1] == '0' && gameBoard.getBoard()[2][2] == '0') {
+      console.log(`${playerTwo.name} has won!`);
+    }
+    
+    // SECOND GROUP
+    else if (gameBoard.getBoard()[0][0] == 'X' && gameBoard.getBoard()[1][0] == 'X' && gameBoard.getBoard()[2][0] == 'X') {
+      console.log(`${playerOne.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[0][0] == '0' && gameBoard.getBoard()[1][0] == '0' && gameBoard.getBoard()[2][0] == '0') {
+      console.log(`${playerTwo.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[0][1] == 'X' && gameBoard.getBoard()[1][1] == 'X' && gameBoard.getBoard()[2][1] == 'X') {
+      console.log(`${playerOne.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[0][1] == '0' && gameBoard.getBoard()[1][1] == '0' && gameBoard.getBoard()[2][1] == '0') {
+      console.log(`${playerTwo.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[0][2] == 'X' && gameBoard.getBoard()[1][2] == 'X' && gameBoard.getBoard()[2][2] == 'X') {
+      console.log(`${playerOne.name} has won!`);
+    }
+    else if  (gameBoard.getBoard()[0][2] == '0' && gameBoard.getBoard()[1][2] == '0' && gameBoard.getBoard()[2][2] == '0') {
+      console.log(`${playerTwo.name} has won!`);
+    }
 
-
-  let kur = 10;
-  while (kur > 0) {
-    playTurn();
-    kur--;
+    // THIRD GROUP
+    else if (gameBoard.getBoard()[0][0] == 'X' && gameBoard.getBoard()[1][1] == 'X' && gameBoard.getBoard()[2][2] == 'X') {
+      console.log(`${playerOne.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[0][0] == '0' && gameBoard.getBoard()[1][1] == '0' && gameBoard.getBoard()[2][2] == '0') {
+      console.log(`${playerTwo.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[0][2] == 'X' && gameBoard.getBoard()[1][1] == 'X' && gameBoard.getBoard()[2][0] == 'X') {
+      console.log(`${playerOne.name} has won!`);
+    }
+    else if (gameBoard.getBoard()[0][2] == '0' && gameBoard.getBoard()[1][1] == '0' && gameBoard.getBoard()[2][0] == '0') {
+      console.log(`${playerTwo.name} has won!`);
+    }
   }
 
+
+  while (gameBoard.checkForWin() == false) {
+    playTurn();
+    console.log(gameBoard.checkForWin());
+
+    if (gameBoard.checkForWin() == true) {
+      checkWinner();
+    }
+    
+  }
+
+  
 }
 
 Game();
